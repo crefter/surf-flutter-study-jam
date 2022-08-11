d1aad3a8a2ed6ec16e33e0bf156cf7f85309acf7
crefter <crefter1@yandex.ru>
2022-08-11 19:15:55 +0700

    refactored and created files: colors, constants

---
+++ lib/features/auth/screens/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract class Colors {
+  static const Color white = Color.fromARGB(255, 252, 253, 246);
+  static const Color green = Color.fromARGB(255, 0, 110, 23);
+  static const Color grey = Color.fromARGB(255, 138, 139, 135);
+  static const Color tangoPink = Color.fromARGB(255, 229, 115, 115);
+  static const Color mineShaft = Color.fromARGB(255, 48, 48, 48);
+}
