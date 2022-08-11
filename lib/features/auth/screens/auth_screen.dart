11b4ff642546c935360ac63fa8384abf2d06d4b5
crefter <crefter1@yandex.ru>
2022-08-11 19:14:43 +0700

    created auth_screen

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:surf_practice_chat_flutter/features/auth/consts.dart';
 import 'package:surf_practice_chat_flutter/features/auth/models/token_dto.dart';
-import 'package:surf_practice_chat_flutter/features/auth/repository/auth_repository.dart';
+import 'package:surf_practice_chat_flutter/features/auth/screens/widgets/auth_text_field.dart';
 import 'package:surf_practice_chat_flutter/features/chat/repository/chat_repository.dart';
 import 'package:surf_practice_chat_flutter/features/chat/screens/chat_screen.dart';
 import 'package:surf_study_jam/surf_study_jam.dart';
+import '../bloc/auth_bloc.dart';
+import 'colors.dart' as colors;
 
 /// Screen for authorization process.
-///
-/// Contains [IAuthRepository] to do so.
 class AuthScreen extends StatefulWidget {
-  /// Repository for auth implementation.
-  final IAuthRepository authRepository;
-
-  /// Constructor for [AuthScreen].
   const AuthScreen({
-    required this.authRepository,
     Key? key,
   }) : super(key: key);
 
@@ -23,10 +20,121 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  // TODO(task): Implement Auth screen.
+  late double _screenHeight;
+  late double _screenWidth;
+  late final TextEditingController _loginController;
+  late final TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _loginController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    Size size = MediaQuery.of(context).size;
+    _screenHeight = size.height;
+    _screenWidth = size.width;
+  }
+
+  @override
+  void dispose() {
+    _loginController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      body: ColoredBox(
+        color: colors.Colors.white,
+        child: BlocConsumer<AuthBloc, AuthState>(
+          listenWhen: (prev, curr) =>
+              curr is AuthStateError || curr is AuthStateSuccess,
+          listener: (context, state) {
+            state.when(
+                notAuth: () => null,
+                error: (message) {
+                  final theme = Theme.of(context)
+                      .textTheme
+                      .titleLarge
+                      ?.copyWith(color: colors.Colors.white);
+                  return ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      padding: EdgeInsets.zero,
+                      content: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Container(
+                            color: colors.Colors.tangoPink,
+                            width:
+                                _screenWidth / AuthConsts.widthSnackBarFactor,
+                            height:
+                                _screenHeight / AuthConsts.heightSnackBarFactor,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(
+                              left: AuthConsts.padding8_0,
+                              right: AuthConsts.padding8_0,
+                            ),
+                            child: Icon(
+                              Icons.error,
+                              color: colors.Colors.tangoPink,
+                            ),
+                          ),
+                          Text(
+                            state.when<String>(
+                                notAuth: () => "",
+                                error: (message) => message,
+                                inProgress: () => "",
+                                success: (_) => ""),
+                            style: theme,
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                inProgress: () => null,
+                success: (token) {
+                  _pushToChat(context, token);
+                });
+          },
+          builder: (context, state) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AuthTextField(
+                  label: "Логин",
+                  icon: Icons.account_circle_outlined,
+                  controller: _loginController,
+                ),
+                AuthTextField(
+                  label: "Пароль",
+                  icon: Icons.lock,
+                  isPassword: true,
+                  controller: _passwordController,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: AuthConsts.defaultPadding,
+                    right: AuthConsts.defaultPadding,
+                  ),
+                  child: _ButtonNext(
+                      screenWidth: _screenWidth,
+                      loginController: _loginController,
+                      passwordController: _passwordController),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
   }
 
   void _pushToChat(BuildContext context, TokenDto token) {
@@ -41,6 +149,43 @@ class _AuthScreenState extends State<AuthScreen> {
           );
         },
       ),
+    );
+  }
+}
+
+class _ButtonNext extends StatelessWidget {
+  const _ButtonNext({
+    Key? key,
+    required double screenWidth,
+    required TextEditingController loginController,
+    required TextEditingController passwordController,
+  })  : _screenWidth = screenWidth,
+        _loginController = loginController,
+        _passwordController = passwordController,
+        super(key: key);
+
+  final double _screenWidth;
+  final TextEditingController _loginController;
+  final TextEditingController _passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: _screenWidth,
+      child: ElevatedButton(
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.all<Color>(colors.Colors.green),
+          ),
+          onPressed: () {
+            context.read<AuthBloc>().add(
+                  AuthEvent.logIn(
+                    _loginController.text,
+                    _passwordController.text,
+                  ),
+                );
+          },
+          child: const Text("ДАЛЕЕ")),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:surf_practice_chat_flutter/features/auth/bloc/auth_bloc.dart';
 import 'package:surf_practice_chat_flutter/features/auth/repository/auth_repository.dart';
+import 'package:surf_practice_chat_flutter/features/auth/repository/token_repository.dart';
 import 'package:surf_practice_chat_flutter/features/auth/screens/auth_screen.dart';
+import 'package:surf_practice_chat_flutter/features/auth/storages/local_secure_storage.dart';
 import 'package:surf_study_jam/surf_study_jam.dart';
 
 void main() async {
@@ -16,9 +20,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: AuthScreen(
-        authRepository: AuthRepository(StudyJamClient()),
+    return BlocProvider(
+      create: (context) => AuthBloc(
+        AuthRepository(StudyJamClient()),
+        TokenRepository(LocalSecureStorage()),
+      ),
+      child: MaterialApp(
+        theme: Theme.of(context).copyWith(primaryColor: Colors.green),
+        home: const AuthScreen(),
       ),
     );
   }
