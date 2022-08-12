c27c2e07770d2181d573ec1aa9c717af9614fc06
crefter <crefter1@yandex.ru>
2022-08-12 12:39:49 +0700

    added chat-feature consts and colors

--- lib/features/auth/consts.dart
+++ lib/features/auth/consts.dart
@@ -1,8 +1,6 @@
 abstract class AuthConsts {
   static const double defaultPadding = 12.0;
-  static const double textFormFieldBorderRadius = 8.0;
   static const double labelFontSize = 16.0;
-  static const double padding8_0 = 8.0;
   static const double widthSnackBarFactor = 20;
   static const double heightSnackBarFactor = 10;
 }
---
+++ lib/features/chat/consts.dart
@@ -0,0 +1,12 @@
+abstract class ChatConsts {
+  static const double heightAppBar = 48;
+  static const double elevationTextFiled = 12;
+  static const double leftTextFieldPadding = 16;
+  static const double bottomTextFieldPadding = 8;
+  static const double chatMessagePadding = 18;
+  static const double gapBetweenUserAvatarAndMessage = 16;
+  static const double defaultGapBetweenWidgets = 4;
+  static const double heightOpenMapButton = 60;
+  static const double offsetOpenMapPicture = 60;
+  static const double fontSizeInAvatar = 24;
+}
